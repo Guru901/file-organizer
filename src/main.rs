493517d588088332ab64c04c3487f0b72629d20a
Guru901 <avinashkaur842@gmail.com>
2025-02-22 22:22:49 +0530

    Implement basic file organization functionality with directory reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let pathname = args
+        .get(1)
+        .expect("Please provide a directory path as an argument.");
+
+    let files: Vec<_> = fs::read_dir(pathname)
+        .expect("Failed to read directory")
+        .collect();
+
+    files.iter().for_each(|f| match f {
+        Ok(entry) => {
+            let path = entry.path();
+            if !path.is_dir() {
+                let extensions = path.extension();
+
+                if let Some(extension) = extensions.clone() {
+                    // make directories
+                    // move files
+                }
+            }
+        }
+        Err(e) => println!("Error: {}", e),
+    });
 }
